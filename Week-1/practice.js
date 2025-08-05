9c7e28982cbf01ced00ccb1e87da44e333df0621
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-08-06 00:05:50 +0900

    practice

---
+++ Week-1/practice.js
@@ -0,0 +1,15 @@
+// function PlusMinus(arr) {
+//   //Write code here
+//   let [pos, neg, zer] = [0, 0, 0];
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > 1) pos++;
+//     else if (arr[i] < 0) neg++;
+//     else if (arr[i] === 0) zer++;
+//   }
+//   let ratio = [pos, neg, zer];
+//   ratio.forEach((val) => {
+//     console.log((val / arr.length).toFixed(6));
+//   });
+// }
+
+// PlusMinus([3, -2, -1, 0, -3, 2]);
